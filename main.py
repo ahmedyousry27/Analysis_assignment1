0b9d5b0c5ca4b08f4529b1012febd8b5c071dc58
ahmedyousry27 <ahmedyousry2002@gmail.com>
2023-11-03 03:04:21 +0100

    graph running time

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import time
+
+from matplotlib import pyplot as plt
+
 
 def iterative_Method_powerNumber(a,n):
     result=1
@@ -14,6 +18,36 @@ def divide_and_conquer_powerNumber(a,n) :
         return a * divide_and_conquer_powerNumber(a, (n-1)//2) * divide_and_conquer_powerNumber(a, (n-1)//2)
 
 # Press the green button in the gutter to run the script.
+def graph():
+
+    common_difference = (10 ** 6 - 1) // 7
+    n_array = [1 + i * common_difference for i in range(8)]
+    iterative_times = []
+    divide_and_conquer_times = []
+
+    for n in n_array:
+        start_time = time.time()
+        iterative_Method_powerNumber(2, n)  # Using base 2, f
+        end_time = time.time()
+        iterative_times.append(end_time - start_time)
+
+        start_time = time.time()
+        divide_and_conquer_powerNumber(2, n)  # Using base 2, for example
+        end_time = time.time()
+        divide_and_conquer_times.append(end_time - start_time)
+
+    # Plotting the results
+    plt.figure(figsize=(10, 6))
+    plt.plot(n_array, iterative_times, label='Iterative Method')
+    plt.plot(n_array, divide_and_conquer_times, label='divide_and_conquer')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlabel('n')
+    plt.ylabel('Time (seconds)')
+    plt.title('Power Number')
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
     print(divide_and_conquer_powerNumber(2,4))
-
+    graph()
